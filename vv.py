c0e2d99076d29a3effb6629c93498b42cce7272c
Supratim <jsupratim089@gmail.com>
2024-08-09 21:16:01 +0530

    ih

--- vv.py
+++ vv.py
@@ -1,16 +1,1 @@
-class Person:
-  def __init__(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
-
-  def printname(self):
-    print(self.firstname, self.lastname)
-
-#Use the Person class to create an object, and then execute the printname method:
-
-x = Person("John", "Doe")
-x.printname()
-
-print(10 + 5)
-print(10 -5)
-print(10 * 5)
+print("Hello")
